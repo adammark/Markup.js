97a000974e0c7b4fafea880a31c2a7d327e15e28
Adam Mark <adammark75@gmail.com>
2011-12-14 08:06:45 -0500

    Added 'groupby' pipe

--- src/pipes/arrays.js
+++ src/pipes/arrays.js
@@ -1,3 +1,42 @@
+/*
+ * Get a random entry from an array.
+ */
 Mark.pipes.rand = function (arr) {
     return arr[Math.floor(Math.random() * arr.length)];
+};
+
+/*
+ * Group an array of objects according to a given property. Returns an array
+ * of objects, each having a "key" (string) and "items" (array).
+ *
+ * Example:
+ *
+ * {{results|groupby>state}}
+ *     <h1>{{key}}</h1>
+ *     <ul>
+ *        {{items}}
+ *            <li>{{zip}}</li>
+ *        {{/items}}
+ *     </ul>
+ * {{/results}}
+ */
+Mark.pipes.groupby = function (arr, key) {
+    var a = [], b = [], i, j, k;
+
+    for (i in arr) {
+        j = arr[i][key];
+        if (a.indexOf(j) === -1) {
+            a.push(j);
+            b.push({ items:[] });
+        }
+    }
+
+    for (i in arr) {
+        j = arr[i][key];
+        k = a.indexOf(j);
+        b[k].key = j;
+        b[k].items.push(arr[i]);
+    }
+
+    return b;
 };
