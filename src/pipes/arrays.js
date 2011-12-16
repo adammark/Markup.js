7add0b298dea407d5c35cada823a552ba761bb95
Adam Mark <adammark75@gmail.com>
2011-12-16 14:05:46 -0500

    Added alphabetical sort pipe

--- src/pipes/arrays.js
+++ src/pipes/arrays.js
@@ -11,32 +11,71 @@ Mark.pipes.rand = function (arr) {
  *
  * Example:
  *
- * {{results|groupby>state}}
- *     <h1>{{key}}</h1>
+ * {{offices|groupby>state}}
+ *     <h1>Offices in {{key}}</h1>
  *     <ul>
  *        {{items}}
- *            <li>{{zip}}</li>
+ *            <li>{{street}}</li>
  *        {{/items}}
  *     </ul>
- * {{/results}}
+ * {{/offices}}
  */
-Mark.pipes.groupby = function (arr, key) {
+Mark.pipes.groupby = function (arr, prop) {
     var a = [], b = [], i, j, k;
 
-    for (i in arr) {
-        j = arr[i][key];
+    for (i = 0; i < arr.length; i++) {
+        j = arr[i][prop];
         if (a.indexOf(j) === -1) {
             a.push(j);
             b.push({ items:[] });
         }
     }
 
-    for (i in arr) {
-        j = arr[i][key];
+    for (i = 0; i < arr.length; i++) {
+        j = arr[i][prop];
         k = a.indexOf(j);
         b[k].key = j;
         b[k].items.push(arr[i]);
     }
 
     return b;
+};
+
+/*
+ * Group an array of objects alphabetically by the given property. Returns
+ * an array of objects, each having a "key" (letter of the alphabet) and
+ * "items" (an array of objects in alphabetical order).
+ *
+ * Example:
+ *
+ * {{contacts|alpha>lastname}}
+ *     <h1>{{key}}</h1>
+ *     <ul>
+ *        {{items}}
+ *            <li>{{lastname}}, {{firstname}}</li>
+ *        {{/items}}
+ *     </ul>
+ * {{/contacts}}
+ */
+Mark.pipes.alpha = function (arr, prop) {
+    var a = [], b = "", i, j;
+
+    for (i = 0; i < arr.length; i++) {
+        j = arr[i][prop].charAt(0).toUpperCase();
+
+        if (b.indexOf(j) === -1) {
+            b += j;
+            a[b.indexOf(j)] = { key: j, items: [] };
+        }
+
+        a[b.indexOf(j)].items.push(arr[i]);
+    }
+
+    for (i = 0; i < a.length; i++) {
+        a[i].items.sort(function (a, b) {
+            return a[prop] > b[prop] ? 1 : -1;
+        });
+    }
+
+    return a;
 };
